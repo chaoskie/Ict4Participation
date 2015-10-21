58ffd7b0f17ac7306ca5429a5e6c13afa16049fc
Lars <l.blom@student.fontys.nl>
2015-10-21 22:31:58 +0200

    afspraken GUI added

---
+++ Ict4Participation/Ict4Participation/Afspraken.cs
@@ -0,0 +1,35 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Afspraken.cs" company="ICT4Participation">
+//     Copyright (c) ICT4Participation. All rights reserved.
+// </copyright>
+// <author>ICT4Participation</author>
+//-----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ict4Participation
+{
+    public partial class Afspraken : Form
+    {
+        private Form previous;
+
+        public Afspraken(Form p)
+        {
+            this.InitializeComponent();
+
+            this.previous = p;
+        }
+
+        private void Afspraken_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.previous.Show();
+        }
+    }
+}
--- Ict4Participation/Ict4Participation/Program.cs
+++ Ict4Participation/Ict4Participation/Program.cs
@@ -16,7 +16,7 @@ namespace Ict4Participation
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Afspraken());
         }
     }
 }
