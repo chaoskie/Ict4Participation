7ea4f6e87d97b4d01bfc978ac684de8e9350df19
Lars <l.blom@student.fontys.nl>
2015-10-21 22:09:49 +0200

    Unit Test Added and filled for: Hashing password (not done) Account (not done)

---
+++ Ict4Participation/Unit_Tests/Acc_Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+using Class_Layer;
+
+namespace Unit_Tests
+{
+    [TestClass]
+    public class Acc_Tests
+    {
+        [TestMethod]
+        public void Create_Acc()
+        {
+            Account acc = new Account(1, "test", (new Location("TestHuis")), Accounttype.Administrator, "asdf.com", "Dit is een test", "M");
+            Assert.IsNotNull(acc);
+        }
+    }
+}
---
+++ Ict4Participation/Unit_Tests/Hash_Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Class_Layer;
+
+namespace Unit_Tests
+{
+    [TestClass]
+    public class Hash_Tests
+    {
+        [TestMethod]
+        public void Test_Hashing()
+        {
+            string SendThis = "WachtwoordHash";
+            string Hashed = PasswordHashing.CreateHash(SendThis);
+            Assert.AreNotEqual(SendThis, Hashed, string.Format("Raw wachtwoord ({0}) is gelijk aan Hashedwachtwoord ({1})", SendThis, Hashed));
+            
+        }
+
+        [TestMethod]
+        public void Hashing_Uniek()
+        {
+            string SendThis = "WachtwoordHash";
+            string Hashed_A = PasswordHashing.CreateHash(SendThis);
+            string Hashed_B = PasswordHashing.CreateHash(SendThis);
+
+            Assert.AreNotEqual(Hashed_A, Hashed_B, string.Format("Gehashed wachtenwoorden met identieke start ({0}) zijn gelijk.", SendThis));
+        }
+
+        [TestMethod]
+        public void Login_Hash()
+        {
+            string SendThis = "WachtwoordHash";
+            string RegisterPass = PasswordHashing.CreateHash(SendThis);
+            Assert.IsTrue(PasswordHashing.ValidatePassword(SendThis, RegisterPass), "Login simulatie was niet succesvol.");
+        }
+
+    }
+}
