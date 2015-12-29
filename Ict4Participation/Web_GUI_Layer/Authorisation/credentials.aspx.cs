22934dece661bdde9c88d988f74461116e48206a
Dings.Rowan R <rowan.dings@student.fontys.nl>
2015-12-29 14:19:46 +0100

    Added filebrowsing for specifc users

---
+++ Ict4Participation/Web_GUI_Layer/Authorisation/credentials.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web_GUI_Layer.Authorisation
+{
+    public partial class credentials : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                // if they came to the page directly, ReturnUrl will be null.
+                if (String.IsNullOrEmpty(Request["ReturnUrl"]))
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+        }
+        protected void Login_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            string username = Login.UserName;
+            string password = Login.Password;
+            if (FormsAuthentication.Authenticate(username, password))
+            {
+                FormsAuthentication.RedirectFromLoginPage(username, false);
+            }
+        }
+    }
+}
