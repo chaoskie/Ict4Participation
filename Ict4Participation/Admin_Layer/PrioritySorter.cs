abba98388121d9080c846015ef2900a7c5e074e5
Dings.Rowan R <rowan.dings@student.fontys.nl>
2016-01-07 15:48:16 +0100

    Added comparers for quick sorting

---
+++ Ict4Participation/Admin_Layer/PrioritySorter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Admin_Layer.Comparers;
+
+namespace Admin_Layer
+{
+    public class PrioritySorter
+    {
+        public static List<Questiondetails> OrderBy(List<Questiondetails> lqd, bool byTitle, bool byDate, bool byUrgency, bool byStatus)
+        {
+            if (byTitle)
+            {
+                lqd.Sort(new IQuestiondetailsTitleComparer());
+            }
+            if (byDate)
+            {
+                lqd.Sort(new IQuestiondetailsDateComparer());
+            }
+            if (byUrgency)
+            {
+                lqd.Sort(new IQuestiondetailsUrgencyComparer());
+            }
+            if (byStatus)
+            {
+                lqd.Sort(new IQuestiondetailsStatusComparer());
+            }
+            return lqd;
+        }
+    }
+}
