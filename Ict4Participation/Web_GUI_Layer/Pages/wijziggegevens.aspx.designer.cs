c6789d9a3c138c869e3ec923d92a4b6582d274d6
Dings.Rowan R <rowan.dings@student.fontys.nl>
2016-01-10 21:32:14 +0100

    Page button fixes

--- Ict4Participation/Web_GUI_Layer/Pages/wijziggegevens.aspx.designer.cs
+++ Ict4Participation/Web_GUI_Layer/Pages/wijziggegevens.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace Web_GUI_Layer.Pages {
     public partial class wijziggegevens {
         
         /// <summary>
+        /// none control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlButton none;
+        
+        /// <summary>
         /// Label1 control.
         /// </summary>
         /// <remarks>
